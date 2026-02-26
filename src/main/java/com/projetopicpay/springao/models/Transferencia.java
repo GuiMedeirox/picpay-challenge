cf762a8904f24f57757f6cc850cb70638529f2c1
guilherme <guimedeiroxdev@gmail.com>
2026-02-25 23:55:53 -0300

    feat: criando a model da Transferencia

---
+++ src/main/java/com/projetopicpay/springao/models/Transferencia.java
@@ -0,0 +1,52 @@
+package com.projetopicpay.springao.models;
+
+import com.projetopicpay.springao.enums.StatusTransferencia;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.lang.annotation.ElementType;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="tb_transferencia")
+@Getter
+@Setter
+public class Transferencia {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+
+    @ManyToOne
+    @JoinColumn(name="pagador_id")
+    private Usuario pagador;
+
+
+    @ManyToOne
+    @JoinColumn(name="recebedor_id")
+    private Usuario recebedor;
+
+
+    @Column(nullable = false)
+    private BigDecimal quantia;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusTransferencia status;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime hora_transacao;
+
+
+}
